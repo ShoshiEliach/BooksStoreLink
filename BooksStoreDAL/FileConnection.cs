d67ae0911fc06b6ce1af6234a5325df70c09e57a
shoshi <s0548566523@gmail.com>
2024-03-05 21:27:02 +0200

    Add project files.

---
+++ BooksStoreDAL/FileConnection.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace BooksStoreDAL
+{
+    public class FileConnection
+    {
+        string filePath = Directory.GetCurrentDirectory() + @"\..\..\..\Src\BookList.txt";
+        public List<BookDetails> ReadAllBooks()
+        {
+            var allLines = File.ReadAllLines(filePath);
+            List<BookDetails> listOfBooks = new List<BookDetails>();
+            foreach (var text in allLines)
+            {
+                string[] detailsAsString = text.Split(',');
+                int id = int.Parse(detailsAsString[0]);
+                string name = detailsAsString[1];
+                int price = int.Parse(detailsAsString[2]);
+                int numberOfPages = int.Parse(detailsAsString[3]);
+                int minAge = int.Parse(detailsAsString[4]);
+                int maxAge = int.Parse(detailsAsString[5]);
+                bool isComics = bool.Parse(detailsAsString[6]);
+                var newBook = new BookDetails(id, name, price, numberOfPages,
+                    minAge, maxAge, isComics);
+                listOfBooks.Add(newBook);
+            }
+            return listOfBooks;
+        }
+        public List<BookDetails> ExpensiveBook()
+        {
+            var allLines = File.ReadAllLines(filePath);
+            List<BookDetails> listOfBooks = new List<BookDetails>();
+            foreach (var text in allLines)
+            {
+                string[] detailsAsString = text.Split(',');
+                int id = int.Parse(detailsAsString[0]);
+                string name = detailsAsString[1];
+                int price = int.Parse(detailsAsString[2]);
+                int numberOfPages = int.Parse(detailsAsString[3]);
+                int minAge = int.Parse(detailsAsString[4]);
+                int maxAge = int.Parse(detailsAsString[5]);
+                bool isComics = bool.Parse(detailsAsString[6]);
+                var newBook = new BookDetails(id, name, price, numberOfPages,
+                    minAge, maxAge, isComics);
+                listOfBooks.Add(newBook);
+            }
+
+            var highCost = from b in listOfBooks
+                           where b.Price > 30
+                           select b;
+            return highCost.ToList();
+            
+
+        }   public List<BookDetails> BookOrderById()
+        {
+            var allLines = File.ReadAllLines(filePath);
+            List<BookDetails> listOfBooks = new List<BookDetails>();
+            foreach (var text in allLines)
+            {
+                string[] detailsAsString = text.Split(',');
+                int id = int.Parse(detailsAsString[0]);
+                string name = detailsAsString[1];
+                int price = int.Parse(detailsAsString[2]);
+                int numberOfPages = int.Parse(detailsAsString[3]);
+                int minAge = int.Parse(detailsAsString[4]);
+                int maxAge = int.Parse(detailsAsString[5]);
+                bool isComics = bool.Parse(detailsAsString[6]);
+                var newBook = new BookDetails(id, name, price, numberOfPages,
+                    minAge, maxAge, isComics);
+                listOfBooks.Add(newBook);
+            }
+
+
+           var orderId = listOfBooks.OrderBy(b => b.Id);
+
+            return orderId.ToList();
+            
+
+        }
+
+        public List<int> ComicsBook()
+        {
+            var allLines = File.ReadAllLines(filePath);
+            List<BookDetails> listOfBooks = new List<BookDetails>();
+            foreach (var text in allLines)
+            {
+                string[] detailsAsString = text.Split(',');
+                int id = int.Parse(detailsAsString[0]);
+                string name = detailsAsString[1];
+                int price = int.Parse(detailsAsString[2]);
+                int numberOfPages = int.Parse(detailsAsString[3]);
+                int minAge = int.Parse(detailsAsString[4]);
+                int maxAge = int.Parse(detailsAsString[5]);
+                bool isComics = bool.Parse(detailsAsString[6]);
+                var newBook = new BookDetails(id, name, price, numberOfPages,
+                    minAge, maxAge, isComics);
+                listOfBooks.Add(newBook);
+            }
+
+            var Comics = from b in listOfBooks
+                           where b.IsComics 
+                           select b.Price;
+            return Comics.ToList();
+
+
+        }
+
+        public List<string> Book9()
+        {
+            var allLines = File.ReadAllLines(filePath);
+            List<BookDetails> listOfBooks = new List<BookDetails>();
+            foreach (var text in allLines)
+            {
+                string[] detailsAsString = text.Split(',');
+                int id = int.Parse(detailsAsString[0]);
+                string name = detailsAsString[1];
+                int price = int.Parse(detailsAsString[2]);
+                int numberOfPages = int.Parse(detailsAsString[3]);
+                int minAge = int.Parse(detailsAsString[4]);
+                int maxAge = int.Parse(detailsAsString[5]);
+                bool isComics = bool.Parse(detailsAsString[6]);
+                var newBook = new BookDetails(id, name, price, numberOfPages,
+                    minAge, maxAge, isComics);
+                listOfBooks.Add(newBook);
+            }
+
+            var Comics = from b in listOfBooks
+                         where b.MinAge<9 && b.MaxAge>9
+                         select b.Name;
+            return Comics.ToList();
+
+
+        }
+    }
+
+}
---
+++ BooksStoreGUI/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BooksStoreGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BooksStoreBL.BookStoreBL booksStoreBL; 
+        public MainWindow()
+        {
+            booksStoreBL = new BooksStoreBL.BookStoreBL();
+
+
+            InitializeComponent();
+            var res = booksStoreBL.GetAllBooks();
+            BooksDataGrid.ItemsSource = res;
+
+          //  תרגיל 1
+            var res1=booksStoreBL.GetExpensiveBooks();
+            BooksDataGrid.ItemsSource = res1;
+
+            //תרגיל 2
+            var res2 = booksStoreBL.GetOrderBooks();
+            BooksDataGrid.ItemsSource = res2;
+
+            //תרגיל 3
+            var res3 = booksStoreBL.GetComicBooks();
+            BooksDataGrid.ItemsSource = res3.Select(x => new { Value = x }).ToList();
+;
+
+            //תרגיל 4
+            var res4 = booksStoreBL.Get9Books();
+              BooksDataGrid.ItemsSource = res4.Select(x => new { Value = x }).ToList();
+
+        }
+    }
+}
