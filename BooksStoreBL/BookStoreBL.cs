f57558daecd45ca726dc10fecebff635f86e6f45
shoshi <s0548566523@gmail.com>
2024-03-05 21:27:02 +0200

    Add project files.

---
+++ BooksStoreBL/BookStoreBL.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BooksStoreDAL;
+using Entities;
+
+namespace BooksStoreBL
+{
+    public class BookStoreBL
+    {
+        List<BookDetails> ListOfBooks;
+        List<BookDetails> ListExpensiveBook;
+        List<BookDetails> orderList;
+        List<int> ComicsList;
+        List<string> Books9List;
+        FileConnection fileConnection;
+        public BookStoreBL()
+        {
+            fileConnection = new FileConnection();
+            ListOfBooks = fileConnection.ReadAllBooks();
+            ListExpensiveBook = fileConnection.ExpensiveBook();
+            orderList = fileConnection.BookOrderById();
+            ComicsList = fileConnection.ComicsBook();
+            Books9List = fileConnection.Book9();
+
+        }
+
+        public IEnumerable<BookDetails> GetAllBooks()
+        {
+            return ListOfBooks;
+        }
+        public IEnumerable<BookDetails> GetExpensiveBooks()
+        {
+            return ListExpensiveBook;
+        }
+        public IEnumerable<BookDetails> GetOrderBooks()
+        {
+            return orderList;
+        }
+        public IEnumerable<int> GetComicBooks()
+        {
+            return ComicsList;
+        }
+        public IEnumerable<string> Get9Books()
+        {
+            return Books9List;
+        }
+    }
+}
---
+++ BooksStoreGUI/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BooksStoreGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BooksStoreBL.BookStoreBL booksStoreBL; 
+        public MainWindow()
+        {
+            booksStoreBL = new BooksStoreBL.BookStoreBL();
+            
+            InitializeComponent();
+            /*var res = booksStoreBL.GetAllBooks();
+            BooksDataGrid.ItemsSource = res;*/
+
+            //תרגיל 1
+            /*var res=booksStoreBL.GetExpensiveBooks();
+            BooksDataGrid.ItemsSource = res;*/
+
+            //תרגיל 2
+            /*var res = booksStoreBL.GetOrderBooks();
+            BooksDataGrid.ItemsSource = res;*/
+
+            //תרגיל 3
+            /*var res = booksStoreBL.GetComicBooks();
+            BooksDataGrid.ItemsSource = res;*/
+
+            //תרגיל 4
+            var res = booksStoreBL.Get9Books();
+            BooksDataGrid.ItemsSource = res;
+        }
+    }
+}
